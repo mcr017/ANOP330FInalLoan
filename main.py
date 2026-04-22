33925bffc63556d1ffec48634a7c907a42518838
mcr017 <148008293+mcr017@users.noreply.github.com>
2026-04-22 19:59:22 -0400

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1u7UaTt2fjtW4iDsHxpQS-ZZNyqOLkSWX
 """
 
-!pip install streamlit
 
 import streamlit as st
 import pandas as pd
